11ea953fb67b5039fc4474c596fff042cb41676b
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-09-02 12:42:16 -0400

    Update and rename Problem_solving_patterns/Sliding_window_pattern.js to Problem_Solving_Patterns/Sliding_Window_Pattern.js

--- Problem_Solving_Patterns/Sliding_Window_Pattern.js
+++ Problem_Solving_Patterns/Sliding_Window_Pattern.js
@@ -79,8 +79,8 @@ function minSubArrayLen(arr, num){
 
 let arr1 = [2, 3, 1, 2, 4, 4];
 let arr2 = [2, 1, 6, 5, 4];
-let arr3 = [3,1,7,11,2,9,8,21,62,33,19];
-let arr4 = [1,4,16,22,5,7,8, 9,10];
+let arr3 = [3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19];
+let arr4 = [1, 4, 16, 22, 5, 7, 8, 9, 10];
 let arr5 = [4, 3, 3, 8, 1, 2, 3];
 
 console.log(minSubArrayLen(arr1, 7)); // 2
