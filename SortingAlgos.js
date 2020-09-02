0cc4f0fc4766e4026b1d1facdc400a2dce70ea87
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-09-02 12:14:46 -0400

    Update SortingAlgos.js

--- SortingAlgos.js
+++ SortingAlgos.js
@@ -78,82 +78,43 @@ function merge(arr1, arr2){
 // QUICK SORT ------------------------------
 // Time: O(n log(n)), Space: O(log(n))
 
-// OPTION 1
-function quickSort(arr, first = 0, last = arr.length-1){
-    let partitionPoint;
-    if (first < last){
-        partitionPoint = partition(arr, first, last);
-        quickSort(arr, first, partitionPoint-1);
-        quickSort(arr, partitionPoint+1, last);
-    }
-}
-// partition helper function
-function partition(arr, first, last){
-    let pivot = arr[first];
-    let small = first; // index of the last small element
-    
-    for (let i = first+1; i <= last; i++){
-        if (arr[i] < pivot){
-            small++;
-            swap(arr[i], arr[small]);
-        }
-    }
-    swap(arr[first], arr[small]);
-    return small;
-}
-// swap helper function
-function swap(num1, num2){
-  let temp = num1;
-  num1 = num2;
-  num2 = temp;
+// Call the pivot helper on the array
+// When the helper returns the updated pivot index, recursively call the pivot helper on the subarray to the left of that idx
+function quickSort(arr, left = 0, right = arr.length-1){
+  if (left < right){
+    let pivotIdx = pivot(arr, left, right);
+    quickSort(arr, left, pivotIdx-1);
+    quickSort(arr, pivotIdx+1, right);
+  }
+  return arr;
 }
 
-// OPTION 2
-function partition(arr, left, right, pivot){
-  // swap elements while left bound <= right bound
-  while (left <= right){
-    // move left pointer until we find
-    // an element that is greater than our pivot element
-    // this element will get "pushed" to the right
-    while (arr[left] < pivot) left++;
-    // move right pointer until we find
-    // an element that is less than our pivot element
-    // this element will get "pushed" to the left
-    while (arr[right] > pivot) right--;
+// Pivot helper/partition
+// Works by selecting one element ('pivot') and finding the index where the pivot should end up in the sorted array
+// It should then rearrange the elements in the array so that all values less than the pivot are moved to the left of the pivot, 
+// all values greater than the pivot are moved to the right of the pivot.
+// The order of elements on either side of picot doesnt matter.
+// When complete, the helpers should return the index of the pivot.
 
-    if (left <= right) {
-      // after swapping elements, move left and right 
-      // pointers towards each other.
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      left++;
-      right--;
+function pivot(arr, start = 0, end = arr.length-1) {
+  let pivot = arr[start];
+  let swapIdx = start;
+  for (let i = start+1; i < arr.length; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
     }
   }
-  // after our left and right pointers cross (i.e. left > right),
-  // the left pointer will mark the division between
-  // elements greater than and less than the pivot element. 
-  return left;
+  swap(arr, start, swapIdx);
+  return swapIdx;
 }
 
-function quickSort(arr, left = 0, right = arr.length-1){
-  // base case: we are done when the left and right bounds meet.
-  if (left >= right) return;
-  // we are choosing the middle element of the current partition
-  // (but the pivot element can be any element in the arr)
-  const pivot = arr[Math.floor((left + right) / 2)];
-
-  // partition the arr around our chosen pivot element.
-  // after each partition, elements greater
-  // than the pivot element will accumulate to the right of the arr,
-  // and elements less than the pivot element will accumulate to the left.
-  // the value of `index` marks this division. 
-  const index = partition(arr, left, right, pivot);
-
-  // recurse on each of the two partitions:
-  // 1) the partition with elements greater than the pivot element.
-  // 2) the partition with elements less than the pivot element.
-  // (the partition in which the pivot element itself lands depends on the 
-  // implementation of the partition function and is not an important detail.)
-  quickSort(arr, left, index - 1);
-  quickSort(arr, index, right);
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  return arr;
 }
+
+const arr = [9,7,8,6,5,3,22,5,4,2,3,10,1];
+console.log(quickSort(arr));
