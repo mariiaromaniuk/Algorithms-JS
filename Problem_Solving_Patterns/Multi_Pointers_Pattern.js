4427cbc387c16a9411b152d29f57328f0d5a73b1
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-10-27 23:16:08 -0400

    Update Multi_Pointers_Pattern.js

--- Problem_Solving_Patterns/Multi_Pointers_Pattern.js
+++ Problem_Solving_Patterns/Multi_Pointers_Pattern.js
@@ -2,20 +2,7 @@
 // first pair where the sum is 0. Return an array that includes both values that sum to zero 
 // or undefined if a pair does not exist.
 
-// OPTION 1 --> O(n^2)
-function sumZero(arr) {
-  if (!arr.length) 
-    return undefined;
-
-  for (let i = 0; i < arr.length; i++){
-    for (let j = i + 1; j < arr.length; j++){
-      if (arr[i] + arr[j] === 0) 
-        return [arr[i], arr[j]];
-    }
-  }
-}
-
-// OPTION 2 --> O(n)
+// Time: O(n)
 function sumZero(arr){
   if (!arr.length) 
     return undefined;
