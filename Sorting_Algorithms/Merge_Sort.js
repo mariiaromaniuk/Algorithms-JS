844f62464da04e415ef01637d4e0e13041c603a8
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-10-28 22:20:10 -0400

    Update Merge_Sort.js

--- Sorting_Algorithms/Merge_Sort.js
+++ Sorting_Algorithms/Merge_Sort.js
@@ -1,9 +1,10 @@
 // Time: O(n log(n)), Space: O(n)
 function mergeSort(arr){
-  if (arr.length <= 1) return arr;
+  if (arr.length <= 1) 
+    return arr;
   let mid = Math.floor(arr.length / 2),
-      left = mergeSort(arr.slice(0, mid)),
-      right = mergeSort(arr.slice(mid));
+  left = mergeSort(arr.slice(0, mid)),
+  right = mergeSort(arr.slice(mid));
 
   return merge(left, right);
 };
