4a21bc871e3eba684ad1af6650c3ef38de3dde34
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-10-28 22:19:44 -0400

    Create Merge_Sort.js

---
+++ Sorting_Algorithms/Merge_Sort.js
@@ -0,0 +1,20 @@
+// Time: O(n log(n)), Space: O(n)
+function mergeSort(arr){
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2),
+      left = mergeSort(arr.slice(0, mid)),
+      right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+function merge(arr1, arr2){
+  let sorted = [];
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) 
+      sorted.push(arr1.shift());
+    else 
+      sorted.push(arr2.shift());
+  };
+  return sorted.concat(arr1.concat(arr2));
+};
