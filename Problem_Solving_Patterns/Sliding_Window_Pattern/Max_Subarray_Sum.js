e7612065dfb4eb1656d5ad5b3c0e79a672b2f36f
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-03 22:26:35 -0500

    Update Max_Subarray_Sum.js

--- Problem_Solving_Patterns/Sliding_Window_Pattern/Max_Subarray_Sum.js
+++ Problem_Solving_Patterns/Sliding_Window_Pattern/Max_Subarray_Sum.js
@@ -1,16 +1,17 @@
-// Given an array of integers and a number, write a function which finds the maximum sum of 
-// a subarray with the length of the number passed to the function.
+// Given an array of positive numbers and a positive number ‘k’, 
+// find the maximum sum of any contiguous subarray of size ‘k’.
 // Note: that a subarray must consist of consecutive elements from the original array.
 
-// OPTION 1 --> O(n^2)
-function maxSubarraySum(arr, num){
-  if (arr.length < num) 
+
+// OPTION 1 --> O(n^2), O(1)
+function max_sub_array_of_size_k(k, arr){
+  if (arr.length < k) 
     return null;
 
   let max = -Infinity;
-  for (let i = 0; i < arr.length - num + 1; i++){
+  for (let i = 0; i < arr.length - k + 1; i++){
     let temp = 0;
-    for (let j = 0; j < num; j++) 
+    for (let j = 0; j < k; j++) 
       temp += arr[i + j];
     if (temp > max) 
       max = temp;
@@ -18,31 +19,45 @@ function maxSubarraySum(arr, num){
   return max;
 };
 
-// OPTION 2 --> O(n)
-function maxSubarraySum(arr, num){
-  if (num > arr.length)
+
+// OPTION 2 --> O(n), O(1)
+function max_sub_array_of_size_k(k, arr){
+  if (k > arr.length)
     return null;
 
   let maxSum = 0;
   let tempSum = 0;
-  for (let i = 0; i < num; i++)
+  for (let i = 0; i < k; i++)
     maxSum += arr[i];
 
   tempSum = maxSum;
-  for (let i = num; i < arr.length; i++){
-    tempSum = tempSum - arr[i-num] + arr[i];
+  for (let i = k; i < arr.length; i++){
+    tempSum = tempSum - arr[i-k] + arr[i];
     maxSum = Math.max(maxSum, tempSum);
   }
   return maxSum;
 }
 
-let arr1 = [1, 2, 5, 2, 8, 1, 5];
-let arr2 = [4, 2, 1, 6];
-let arr3 = [];
+
+// OPTION 3 --> O(n), O(1)
+function max_sub_array_of_size_k(k, arr) {
+  let maxSum = 0,
+    windowSum = 0,
+    windowStart = 0;
+
+  for (window_end = 0; window_end < arr.length; window_end++) {
+    windowSum += arr[window_end]; // add the next element
+    // slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if (window_end >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= arr[windowStart]; // subtract the element going out
+      windowStart += 1; // slide the window ahead
+    }
+  }
+  return maxSum;
+}
 
 // Test
-console.log(maxSubarraySum(arr1, 2)); // 10
-console.log(maxSubarraySum(arr1, 4)); // 17
-console.log(maxSubarraySum(arr2, 1)); // 6
-console.log(maxSubarraySum(arr2, 4)); // 13
-console.log(maxSubarraySum(arr3, 4)); // null
+console.log(`Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])}`);
+console.log(`Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])}`);
+console.log(`Maximum sum of a subarray of size K: ${max_sub_array_of_size_k(2, [])}`);
