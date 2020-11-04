78254b53fb55e8cee2fb4393de3ae8adf1a5cdbd
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-04 15:32:19 -0500

    Create Squaring_Sorted_Array.js

---
+++ Problem_Solving_Patterns/Two_Pointers/Squaring_Sorted_Array.js
@@ -0,0 +1,30 @@
+// Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
+// Input: [-2, -1, 0, 2, 3]
+// Output: [0, 1, 4, 4, 9]
+
+// Time: O(n), Space: O(1).
+function make_squares(arr) {
+  const n = arr.length;
+  squares = Array(n).fill(0);
+  let highestSquareIdx = n - 1;
+  let left = 0,
+    right = n - 1;
+  while (left <= right) {
+    let leftSquare = arr[left] * arr[left],
+      rightSquare = arr[right] * arr[right];
+    if (leftSquare > rightSquare) {
+      squares[highestSquareIdx] = leftSquare;
+      left += 1;
+    } else {
+      squares[highestSquareIdx] = rightSquare;
+      right -= 1;
+    }
+    highestSquareIdx -= 1;
+  }
+
+  return squares;
+}
+
+// Test
+console.log(`Squares: ${make_squares([-2, -1, 0, 2, 3])}`);
+console.log(`Squares: ${make_squares([-3, -1, 0, 1, 2])}`);
