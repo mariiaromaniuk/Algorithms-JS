66909caf94787dfe7e561b2c1bc19016774c3cf6
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-05 14:56:45 -0500

    Update Hash_Table.js

--- Data_Structures/Hash_Table.js
+++ Data_Structures/Hash_Table.js
@@ -3,7 +3,7 @@ class HashTable {
     this.keyMap = new Array(size);
   }
 
-  _hash(key) {
+  hash(key) {
     let total = 0;
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
@@ -13,19 +13,21 @@ class HashTable {
     }
     return total;
   }
+  
   set(key,value){
-    let index = this._hash(key);
-    if(!this.keyMap[index]){
+    let index = this.hash(key);
+    if (!this.keyMap[index]){
       this.keyMap[index] = [];
     }
     this.keyMap[index].push([key, value]);
   }
+  
   get(key){
-    let index = this._hash(key);
-    if(this.keyMap[index]){
-      for(let i = 0; i < this.keyMap[index].length; i++){
-        if(this.keyMap[index][i][0] === key) {
-          return this.keyMap[index][i][1]
+    let index = this.hash(key);
+    if (this.keyMap[index]){
+      for (let i = 0; i < this.keyMap[index].length; i++){
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
         }
       }
     }
@@ -33,6 +35,7 @@ class HashTable {
   }
 }
 
+// Test
 let ht = new HashTable(17);
 ht.set("maroon","#800000")
 ht.set("yellow","#FFFF00")
