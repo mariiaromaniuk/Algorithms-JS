3980741285811d50af3d4daac3caa3f5fee62606
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-08 16:40:20 -0500

    Update Tree_Traversal.js

--- Data_Structures/Tree_Traversal.js
+++ Data_Structures/Tree_Traversal.js
@@ -39,6 +39,7 @@ class BinarySearchTree {
   }
 
   // BREADTH FIRST SEARCH
+  // Use less space in the call stack (gueue) for thin and long trees
   // 1. create a queue & a variable (aaray) to store the values of nodes visited
   // 2. place the root node in the queue
   // 3. loop as long as there is anything in the queue
@@ -49,6 +50,7 @@ class BinarySearchTree {
   
   // To implement Queue we can simply use an array (instead of Queue class) + arr.push() and arr.shift()
   // arr.shift() → removes the first element from an array and returns that removed element.
+  
   BFS(){
     let node = this.root,
         visited = [],
@@ -67,6 +69,7 @@ class BinarySearchTree {
    }
 
   // DEPTH FIRST SEARCH
+  // Use less space in the call stack (gueue) for wide trees
   // 1. create a variable to sotre the values of the nodes visited
   // 2. store the root of the BST in a variable called current
   // 3. write a helper function that accepts a node
@@ -75,9 +78,11 @@ class BinarySearchTree {
   // 6. if the node has a right property, call the helper function with the right prperty on the node
   // 7. invoke the helper function with the current variable
   // 8. return the array of values 
-  DFSPreOrder(){
+ 
+  // DFS Pre-order (recursive, returns array of all values)
+  // Used to duplicate or flatten the tree
+  DFS_PreOrder(){
     let visited = [];
-    let current = this.root;
     function traverse(node) {
       visited.push(node.value);
       if (node.left) 
@@ -85,39 +90,69 @@ class BinarySearchTree {
       if (node.right) 
          traverse(node.right);
     }
-    traverse(current)
-    return visited;
-  }
-
-  DFSPostOrder(){
-    let visited = [];
-    let current = this.root;
-    function traverse(node) {
-      if (node.left) 
-         traverse(node.left);
-      if (node.right) 
-         traverse(node.right);
-      visited.push(node.value);
-    }
-    traverse(current);
-    return visited;
-  }
-
-  DFSInOrder() {
-    let visited = [];
-    let current = this.root;
-    function traverse(node) {
-      if (node.left) 
-         traverse(node.left);
-      visited.push(node.value);
-      if (node.right) 
-         traverse(node.right);
-    }
-    traverse(current);
+    traverse(this.root);
     return visited;
   }
   
+  // DFS In-order (recursive, returns array of all values)
+  // Sorts values in ascending order (for BST)
+  DFS_InOrder() {
+    let visited = [];
+    function traverse(node) {
+      if (node.left) 
+         traverse(node.left);
+      visited.push(node.value);
+      if (node.right) 
+         traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  // DFS Post-order (recursive, returns array of all values)
+  DFS_PostOrder(){
+    let visited = [];
+    function traverse(node) {
+      if (node.left) 
+         traverse(node.left);
+      if (node.right) 
+         traverse(node.right);
+      visited.push(node.value);
+    }
+    traverse(this.root);
+    return visited;
+  }
 }
+
+// DFS Pre-order (recursive, void, just prints)
+// Used to duplicate or flatten the tree
+function preOrder(node) {
+  if (node !== null) {
+    console.log(node.val);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+}
+
+// DFS In-order (recursive, void, just prints)
+// Sorts values in ascending order (for BST)
+function inOrder(node) {
+  if (node !== null) {
+    inOrder(node.left);
+    console.log(node.val);
+    inOrder(node.right);
+  }
+}
+
+// DFS Post-order (recursive, void, just prints)
+function postOrder(node) {
+  if (node !== null) {
+    postOrder(node.left);
+    postOrder(node.right);
+    console.log(node.val);
+  }
+}
+
 
 // Test
 var tree = new BinarySearchTree();
@@ -127,6 +162,6 @@ tree.insert(15);
 tree.insert(3);
 tree.insert(8);
 tree.insert(20);
-console.log("pre", tree.DFSPreOrder());
-console.log("post", tree.DFSPostOrder());
-console.log("in", tree.DFSInOrder());
+console.log("Pre:", tree.DFS_PreOrder());
+console.log("Post: ", tree.DFS_PostOrder());
+console.log("In: ", tree.DFS_InOrder());
