b593825aa6cd6f0ae35b9b776713f9296770eb9a
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-08 15:48:45 -0500

    Update Tree_Traversal.js

--- Data_Structures/Tree_Traversal.js
+++ Data_Structures/Tree_Traversal.js
@@ -11,87 +11,70 @@ class BinarySearchTree {
     this.root = null;
   }
   
-  insert(value) {
-        let newNode = new Node(value)
-        if (!this.root) {
-            this.root = newNode;
-            return this;
-        } else {
-            let current = this.root
-            while (true) {
-                if (value === current.value) 
-                   return undefined;
-                if (value < current.value) {
-                    if (current.left === null) {
-                        current.left = newNode;
-                        return this;
-                    } else {
-                        current = current.left;
-                    }
-                } else if (value > current.value) {
-                    if (current.right === null) {
-                        current.right = newNode;
-                        return this;
-                    } else {
-                        current = current.right;
-                    }
-                }
-            }
-        }
-
+  // Insert value to the tree
+  insert(val) {
+    const node = new Node(val);
+    if (!this.root){
+      this.root = node;
+      return this;
     }
-
-  find(val) {
-        let current = this.root;
-        let found = true;
-        if (!this.root) 
-           return false;
-        while (current & !found) {
-            if (val > current.value) {
-                current = current.right;
-            } else if (val < current.value) {
-                current = current.left;
-            } else {
-                return true;
-            }
-        }
-        return false;
+    else {
+      let p = this.root;
+      if (val === p.val){
+        return undefined;
+      } 
+      else if (val < p.val){
+        while (val < p.val && p.left)
+          p = p.left;
+        node.left = p.left;
+        p.left = node;
+      } 
+      else {
+        while (val > p.val && p.right)
+          p = p.right;
+        node.right = p.right;
+        p.right = node;
+      }
     }
+  }
 
-  // Breadth First Search
-  // 1. create a queue & a variable to store the values of nodes visited
+  // BREADTH FIRST SEARCH
+  // 1. create a queue & a variable (aaray) to store the values of nodes visited
   // 2. place the root node in the queue
   // 3. loop as long as there is anything in the queue
   // 4. dequeue a node from the queue and push the value of the node into the variable that stores the nodes
   // 5. if there is a left property on the node dequeued, add it to the queue
   // 6. if there is a right property on teh node dequeued - add it to the queue
   // 7. return the variable that stores the values
-  BFS() {
-    let q = [this.root];
-    let visited = [];
-    let val = this.root;
-    while (q.length > 0) {
-      val = q.shift();
-      visited.push(val);
-      if (val.left) {
-        q.push(val.left);
+  
+  // To implement Queue we can simply use an array (instead of Queue class) + arr.push() and arr.shift()
+  // arr.shift() → removes the first element from an array and returns that removed element.
+  BFS(){
+    let node = this.root,
+        visited = [],
+        queue = []; 
+    queue.push(node);
+    
+    while (queue.length){
+      node = queue.shift();
+      visited.push(node.val);
+      if (node.left) 
+         queue.push(node.left);
+      if (node.right) 
+         queue.push(node.right);
       }
-      if (val.right) {
-        q.push(val.right);
-      }
-    }
     return visited;
-  }
+   }
 
-  // Depth First Search Pre-order
-  //create a variable to sotre the values of the nodes visited
-  //store the root of the BST in a variable called current
-  //write a helper function that accepts a node
-  //push the value of the node to the variable that stores the values
-  //if the node has a left property, call the helper function with the left property on the node
-  //if the node has a right property, call the helper function with the right prperty on the node
-  //invoke the helper function with the current variable
-  //return the array of values 
+  // DEPTH FIRST SEARCH
+  // 1. create a variable to sotre the values of the nodes visited
+  // 2. store the root of the BST in a variable called current
+  // 3. write a helper function that accepts a node
+  // 4. push the value of the node to the variable that stores the values
+  // 5. if the node has a left property, call the helper function with the left property on the node
+  // 6. if the node has a right property, call the helper function with the right prperty on the node
+  // 7. invoke the helper function with the current variable
+  // 8. return the array of values 
   DFSPreOrder(){
     let visited = [];
     let current = this.root;
