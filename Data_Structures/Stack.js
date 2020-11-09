b338c743d7a5ba58c976c7982c2d8e3bf55f0191
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-08 19:41:37 -0500

    Update Stack.js

--- Data_Structures/Stack.js
+++ Data_Structures/Stack.js
@@ -1,3 +1,63 @@
+// Array Implementation of the Stack - Last In First Out
+class Stack {
+  constructor(){
+    // array in which we store the value
+    this.data = [];
+    // points to the top element index
+    this.top = 0;
+  }
+
+  // Add value to the top of the stack
+  push(val){
+    this.data[this.top] = val;
+    this.top = this.top + 1;
+  }
+  
+  // Remove value from the top of the stack
+  pop(){
+    this.top = this.top - 1;
+    return this.data.pop(); // removes the last element
+  }
+  
+  // Get the top element of the stack without removing it
+  peek(){
+    return this.data[this.top - 1];
+  }
+  
+  // Check if stack is empty
+  isEmpty(){
+    return this.data.length === 0;
+  }
+  
+  // Get the length of the stack
+  length(){
+    return this.data.length;
+  }
+  
+  // Print out values of the stack
+  print(){
+    let top = this.top - 1; // because top points to index where new element to be inserted
+    while (top >= 0) { // print up to 0th index
+      console.log(this.data[top]);
+      top--;
+    }
+  }
+  
+  // Reverse the stack
+  reverse(){
+    this.reverseIdx(this.top - 1);
+  }
+  
+  reverseIdx(index){
+    if(index != 0)
+      this.reverseIdx(index - 1);
+    console.log(this.data[index]);
+  }
+}
+
+
+// -----------------------------------------------------------
+// Linked List Implementation of the Stack - Last In First Out
 class Node {
   constructor(val){
     this.val = val;
@@ -5,7 +65,6 @@ class Node {
   }
 }
 
-// First In Last Out
 class Stack {
   constructor() {
     this.first = null;
@@ -13,7 +72,7 @@ class Stack {
     this.size = 0;
   }
 
-  // Add value to the beginning of the stack
+  // Add value to the top of the stack
   push(val){
     let node = new Node(val);
     if (this.size === 0){
@@ -27,7 +86,7 @@ class Stack {
     return ++this.size;
   }
   
-  // Remove value from beginning of the stack
+  // Remove value from the top of the stack
   pop(){
     if (!this.first) 
       return null;
