3eedce9f760d75140a1775e7a92df32cac3dc155
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-08 19:41:09 -0500

    Update Queue.js

--- Data_Structures/Queue.js
+++ Data_Structures/Queue.js
@@ -1,3 +1,46 @@
+// Array Implementation of the Queue - First In First Out
+class Queue {
+  constructor(){
+    // array in which we store the value
+    this.data = [];
+  }
+
+  // Add value to the top of the queue
+  enqueue(val){
+    this.data.push(val);
+  }
+  
+  // Remove value from the top of the queue
+  dequeue(){
+    return this.data.shift();
+  }
+  
+  // Get the element at the front of the queue without removing it
+  peek(){
+    return !this.isEmpty() ? this.data[0] : undefined;
+  }
+  
+  // Check if queue is empty
+  isEmpty(){
+    return this.data.length === 0;
+  }
+  
+  // Get the length of the queue
+  length(){
+    return this.data.length;
+  }
+  
+  // Reverse the queue
+  reverse(index){
+    if (index != 0)
+      this.reverse(index - 1);
+    console.log(this.data[index]);
+  }
+}
+
+
+// -------------------------------------------------------------
+//  Linked List Implementation of the Queue - First In First Out
 class Node {
   constructor(val){
     this.val = val;
@@ -5,7 +48,6 @@ class Node {
   }
 }
 
-// First In First Out
 class Queue {
   constructor() {
     this.first = null;
