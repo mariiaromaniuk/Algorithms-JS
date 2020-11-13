7a3000eaf6d78b8c83a140509f99387e765e5a15
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-12 23:05:10 -0500

    Update Hash_Table.js

--- Data_Structures/Hash_Table/Hash_Table.js
+++ Data_Structures/Hash_Table/Hash_Table.js
@@ -62,15 +62,15 @@ class HashTable {
 
 // Test
 let ht = new HashTable(17);
-ht.set("maroon","#800000")
-ht.set("yellow","#FFFF00")
-ht.set("olive","#808000")
-ht.set("salmon","#FA8072")
-ht.set("lightcoral","#F08080")
-ht.set("mediumvioletred","#C71585")
-ht.set("plum","#DDA0DD")
-ht.set("purple","#DDA0DD")
-ht.set("violet","#DDA0DD")
+ht.set("maroon","#800000");
+ht.set("yellow","#FFFF00");
+ht.set("olive","#808000");
+ht.set("salmon","#FA8072");
+ht.set("lightcoral","#F08080");
+ht.set("mediumvioletred","#C71585");
+ht.set("plum","#DDA0DD");
+ht.set("purple","#DDA0DD");
+ht.set("violet","#DDA0DD");
 
 
 ht.keys().forEach(function(key){
