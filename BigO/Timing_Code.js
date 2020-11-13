c611107b13ab7a93b187f924b26208a05226f320
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-12 23:59:07 -0500

    Update Timing_Code.js

--- BigO/Timing_Code.js
+++ BigO/Timing_Code.js
@@ -23,3 +23,9 @@ var time1 = performance.now();
 addUpTo(1000000000);
 var time2 = performance.now();
 console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
+
+
+// Different machines will record different times
+// The same machine will record different times! 
+// For fast algorithms, speed measurements may not be precise enough?
+// So it is better to count the number of operations rather than time the code
