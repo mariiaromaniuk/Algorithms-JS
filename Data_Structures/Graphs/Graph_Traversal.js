711030a87cf30662ddc5e1203aed45ea6c9b8bf9
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-12 23:04:28 -0500

    Update Graph_Traversal.js

--- Data_Structures/Graphs/Graph_Traversal.js
+++ Data_Structures/Graphs/Graph_Traversal.js
@@ -139,5 +139,33 @@ class Graph {
     }
     return result;
   }
-
 }
+
+// Test
+let g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
+// Queue: []
+// Result: [A, B, C, D, E, F]
