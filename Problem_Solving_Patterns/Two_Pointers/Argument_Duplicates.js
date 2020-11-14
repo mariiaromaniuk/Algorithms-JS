97828004b6a43edab1bcbd2812c6dcdb3d5fb71b
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-14 13:41:05 -0500

    Update Argument_Duplicates.js

--- Problem_Solving_Patterns/Two_Pointers/Argument_Duplicates.js
+++ Problem_Solving_Patterns/Two_Pointers/Argument_Duplicates.js
@@ -1,7 +1,7 @@
 // Write a function which accepts a variable number of arguments, and checks whether there 
 // are any duplicates among the arguments passed in.
 
-// OPTION 1
+// OPTION 1 --> O(n log n)
 function areThereDuplicates(...args){
   args.sort();
   let start = 0;
@@ -16,7 +16,18 @@ function areThereDuplicates(...args){
   return false;
 }
 
-// OPTION 2
+// OPTION 2 --> O(n)
+function areThereDuplicates(...args){
+  let arr = [...args];
+  let map = {};
+  for (let i of arr){
+      if (!map[i]) map[i] = 1;
+      else return true;
+  }
+  return false;
+}
+
+// OPTION 3 --> O(n)
 function areThereDuplicates(arguments){
   return new Set(arguments).size !== arguments.length;
 }
