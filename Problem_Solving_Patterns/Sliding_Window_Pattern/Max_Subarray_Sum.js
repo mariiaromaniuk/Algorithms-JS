07183e01077fbd96e7642cb142a093e71326a620
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-13 19:21:50 -0500

    Update Max_Subarray_Sum.js

--- Problem_Solving_Patterns/Sliding_Window_Pattern/Max_Subarray_Sum.js
+++ Problem_Solving_Patterns/Sliding_Window_Pattern/Max_Subarray_Sum.js
@@ -25,18 +25,21 @@ function max_sub_array_of_size_k(k, arr){
 
 
 // OPTION 2 --> O(n), O(1)
-function max_sub_array_of_size_k(k, arr) {
+function max_sub_array_of_size_k(k, arr){
+  if (k > arr.length)
+    return null;
+  
   let maxSum = 0,
     windowSum = 0,
     windowStart = 0;
 
-  for (window_end = 0; window_end < arr.length; window_end++) {
-    windowSum += arr[window_end]; // add the next element
+  for (windowEnd = 0; windowEnd < arr.length; windowEnd++){
+    windowSum += arr[windowEnd]; // add the next element
     // slide the window, we don't need to slide if we've not hit the required window size of 'k'
-    if (window_end >= k - 1) {
+    if (windowEnd >= k - 1){
       maxSum = Math.max(maxSum, windowSum);
       windowSum -= arr[windowStart]; // subtract the element going out
-      windowStart += 1; // slide the window ahead
+      windowStart++; // slide the window ahead
     }
   }
   return maxSum;
