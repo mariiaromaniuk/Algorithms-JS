7dd8baea35ff33fe31418e1c5a530c480bc54930
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-14 13:19:31 -0500

    Update Same_Frequency_of_Digits.js

--- Problem_Solving_Patterns/Frequency_Counter_Pattern/Same_Frequency_of_Digits.js
+++ Problem_Solving_Patterns/Frequency_Counter_Pattern/Same_Frequency_of_Digits.js
@@ -1,6 +1,7 @@
 // Given two positive integers, find out if the two numbers have the same frequency of digits.
 // Your solution MUST have time complexity: O(n)
 
+// OPTION 1
 function sameFrequency(int1, int2){
   let int1Str = int1.toString()
   let int2Str = int2.toString();
@@ -23,6 +24,28 @@ function sameFrequency(int1, int2){
       return false;
     }
 }
+
+// OPTION 2
+function sameFrequency(int1, int2){
+  let int1Str = int1.toString()
+  let int2Str = int2.toString();
+
+  if (int1Str.length !== int2Str.length)
+    return false;
+
+  const mapInt1 = {};
+  for (let char of int1Str)
+    mapInt1[char] = (mapInt1[char] || 0) + 1;
+
+  for (let i of int2Str){
+    if (!mapInt1[i])
+       return false;
+    else
+       mapInt1[i] = mapInt1[i] - 1;
+    }
+  return true;
+}
+
 
 // Test
 console.log(sameFrequency(182, 281)) // true;
