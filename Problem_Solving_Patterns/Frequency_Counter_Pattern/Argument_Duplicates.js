a8918f9c8901dd5ff2b29785b3807e6a26fd65d8
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-15 00:54:03 -0500

    Update Argument_Duplicates.js

--- Problem_Solving_Patterns/Frequency_Counter_Pattern/Argument_Duplicates.js
+++ Problem_Solving_Patterns/Frequency_Counter_Pattern/Argument_Duplicates.js
@@ -1,14 +1,13 @@
 // Write a function which accepts a variable number of arguments, and checks whether there 
 // are any duplicates among the arguments passed in.
 
-function areThereDuplicates(){
-  const collection = {};
-
-  for (let values in arguments)
-    collection[arguments[values]] = (collection[arguments[values]] || 0) + 1;
-
-  for (let key in collection){
-    if (collection[key] > 1)
+function areThereDuplicates(...args) {
+  let map = {};
+  for (let values in args)
+    map[args[values]] = (map[args[values]] || 0) + 1;
+  
+  for (let key in map){
+    if (map[key] > 1)
       return true;
   }
     return false;
