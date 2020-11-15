05fea3e74ec2942d43a7c6eae94ef7d21eff8dc9
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-15 18:02:27 -0500

    Update Advanced_Recursive_Problems.js

--- Recursion/Advanced_Recursive_Problems.js
+++ Recursion/Advanced_Recursive_Problems.js
@@ -1,8 +1,8 @@
 // REVERSE STRING
 // Write a function which accepts a string and returns a new string in reverse.
 function reverse(str){
-	if (str.length <= 1) return str;
-	return reverse(str.slice(1)) + str[0];
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
 }
 
 // IS PALINDROME
