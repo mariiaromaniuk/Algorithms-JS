23c35b722bcdb955a13c813c5bc32bb07e569ac8
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-11-15 00:18:29 -0500

    Update No_Repeat_Substring.js

--- Problem_Solving_Patterns/Sliding_Window_Pattern/No_Repeat_Substring.js
+++ Problem_Solving_Patterns/Sliding_Window_Pattern/No_Repeat_Substring.js
@@ -2,33 +2,36 @@
 // Input: String="aabccbb"
 // Output: 3  -> the longest substring without any repeating characters is "abc".
 
-function no_repeat_substring(str) {
-  let windowStart = 0,
-    maxLength = 0,
-    charIndexMap = {};
-
-  // try to extend the range [windowStart, windowEnd]
-  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-    const rightChar = str[windowEnd];
+function no_repeat_substring(str){
+  let start = 0,
+      maxLength = 0,
+      charMap = {};
+  // try to extend the range [start, end]
+  for (let end = 0; end < str.length; end++){
+    const rightChar = str[end];
     // if the map already contains the 'rightChar', shrink the window from the beginning so that
     // we have only one occurrence of 'rightChar'
-    if (rightChar in charIndexMap) {
-      // this is tricky; in the current window, we will not have any 'rightChar' after its previous index
-      // and if 'windowStart' is already ahead of the last index of 'rightChar', we'll keep 'windowStart'
-      windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+    if (rightChar in charMap){
+      // in the current window, we will not have any 'rightChar' after its previous index
+      // and if 'start' is already ahead of the last index of 'rightChar', we'll keep 'start'
+      start = Math.max(start, charMap[rightChar] + 1);
     }
     // insert the 'rightChar' into the map
-    charIndexMap[rightChar] = windowEnd;
+    charMap[rightChar] = end;
     // remember the maximum length so far
-    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+    maxLength = Math.max(maxLength, end - start + 1);
   }
   return maxLength;
 }
 
 // Test
-console.log(`Length of the longest substring: ${no_repeat_substring('aabccbb')}`);
-console.log(`Length of the longest substring: ${no_repeat_substring('abbbb')}`);
-console.log(`Length of the longest substring: ${no_repeat_substring('abccde')}`);
+console.log(findLongestSubstring('')); // 0 
+console.log(no_repeat_substring('rithmschool')); // 7
+console.log(no_repeat_substring('thisisawesome')); // 6
+console.log(no_repeat_substring('thecatinthehat')); // 7
+console.log(no_repeat_substring('bbbbbb')); // 1 
+console.log(no_repeat_substring('longestsubstring')); // 8
+console.log(no_repeat_substring('thisishowwedoit')); // 6
 
 
 // Time: O(n), where n is the number of letters in the input string.
