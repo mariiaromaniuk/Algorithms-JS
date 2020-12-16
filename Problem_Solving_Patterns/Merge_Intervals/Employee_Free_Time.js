2bcd430b34e3f0ebc6a1fc9d019c138e6bfec242
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-12-15 22:24:12 -0500

    Create Employee_Free_Time.js

---
+++ Problem_Solving_Patterns/Merge_Intervals/Employee_Free_Time.js
@@ -0,0 +1,15 @@
+/*
+ Employee Free Time (hard)
+ For K employees, we're given a list of intervals representing the working hours of each employee.
+ Find out if there is a free interval that is common to all employees. You can assume that each list 
+ of employee working hours is sorted on the start time.
+ 
+ Example 1: [[[1,3], [5,6]], [[2,3], [6,8]]];
+ Output: [3,5] --> Both employees are free between [3,5].
+ 
+ Example 2: [[[1,3], [9,12]], [[2,4], [6,8]]];
+ Output: [4,6], [8,9]
+ 
+ Example 3: [[[1,3], [2,4]], [[3,5], [7,9]]];
+ Output: [5,7]
+*/
